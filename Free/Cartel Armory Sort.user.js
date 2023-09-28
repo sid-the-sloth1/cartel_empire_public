a0a26eed2635d2d53e90c9066940c7ca45682f98
Hardy <blahblahbrian@protonmail.com>
2023-09-28 07:40:48 +0530

    Add CSS rule for hand cursor

--- Free/Cartel Armory Sort.user.js
+++ Free/Cartel Armory Sort.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         CE: Cartel Armory Sort
 // @namespace    hardy.ce.armory.sort
-// @version      0.1
+// @version      0.2
 // @description  Makes the table on Cartel Armory Page Sortable
 // @author       Hardy [1345]
 // @match        https://cartelempire.online/Cartel/Armory
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=cartelempire.online
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (function() {
@@ -17,6 +18,7 @@
     let headerIndex = 0;
     for (const header of headerRow.children) {
         if (headerIndex === 0 || headerIndex === 1) {
+            header.classList.add("hover-hand");
             header.innerHTML += `<svg style="margin-left:3px;" xmlns="http://www.w3.org/2000/svg" width="10" height="13" viewBox="0 0 1024 1408"><path fill="currentColor" d="M1024 896q0 26-19 45l-448 448q-19 19-45 19t-45-19L19 941Q0 922 0 896t19-45t45-19h896q26 0 45 19t19 45zm0-384q0 26-19 45t-45 19H64q-26 0-45-19T0 512t19-45L467 19q19-19 45-19t45 19l448 448q19 19 19 45z"/></svg>`;
             header.addEventListener("click", (event) => {
                 sort(header.innerText.trim().toLowerCase());
@@ -61,4 +63,7 @@
             cont.insertBefore(cont.querySelector(`[hardy-sort="${num}"]`), last);
         }
     }
+    GM_addStyle(`.hover-hand:hover{
+            cursor:grab;
+        }`)
 })();
